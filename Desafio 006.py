d2f12a199326674e8da26afb3622507e7fb81c5b
Cahuã <88219374+cahuamalveira@users.noreply.github.com>
2021-11-16 16:12:39 -0300

    Update Desafio 006.py

--- Desafio 006.py
+++ Desafio 006.py
@@ -1,5 +1,5 @@
-n = int(input('Digita um número ai seu porra:'))
+n = int(input('Digita um número:'))
 dob = n*2
 tri = n*3
 rqd = n**(1/2)
-print (' O dobro dessa merda de {} é: {} \n O triplo dessa merda de {} é: {} \n A raiz quadrada dessa porra de {} é: {}' .format (n, dob, n, tri, n, rqd))
+print (' O dobro de {} é: {} \n O triplo de {} é: {} \n A raiz quadrada de {} é: {}' .format (n, dob, n, tri, n, rqd))
