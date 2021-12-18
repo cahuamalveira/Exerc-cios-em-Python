07b5de0a87af680a835ff5d1a1cbe84840673c4d
Cahuã <88219374+cahuamalveira@users.noreply.github.com>
2021-12-18 00:54:24 -0300

    Desafio 102

--- Desafio 102.py
+++ Desafio 102.py
@@ -1,5 +1,14 @@
-#https://youtu.be/84jUX96cs7Q
-"""Crie um programa que tenha uma função fatorial() que receba dois parâmetros:
-o primeiro que indique o número a calcular e
-outro chamado show, que será um valor lógico (opcional) indicando se será mostrado ou não na tela o processo
-de cálculo do fatorial."""
+def fatorial(n, show=True):
+    f = 1
+    for c in range(n, 0, -1):
+        if show:
+            print(f'{c}', end='')
+            if c > 1:
+                print(' x ', end='')
+            else:
+                print(' = ', end='')
+        f *= c
+    return f
+
+
+print(fatorial(5, show=True))
